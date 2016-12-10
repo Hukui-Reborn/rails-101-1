6d102d6081ad826f75cc6df047a2096111c9e725
hukui <tckuikui1234@gmail.com>
2016-12-10 13:22:36 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -28,11 +28,14 @@ def destroy
   flash[:alert] = "Group deleted"
   redirect_to groups_path
 end
+
 def create
   @group = Group.new(group_params)
-  @group.save
-
-  redirect_to groups_path
+  if @group.save
+    redirect_to groups_path
+  else
+    render :new
+  end
 end
 
 private
